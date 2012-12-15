294bbbcf0dca1576554fffab7770582568bced77
Niclas Olofsson <n@niclasolofsson.se>
2012-12-15 02:03:46 +0100

    Made some fields non required

--- orkester/sof/orkester/models.py
+++ orkester/sof/orkester/models.py
@@ -26,27 +26,26 @@ class Orchestra(models.Model):
     use_short_name = models.CharField("Är det ok att använda förkortningen i spelschemat?",
                                       choices=YESNO, max_length=5)
     sof_count = models.PositiveSmallIntegerField("Hur många gånger har ni deltagit i SOF?")
-    look_forward_to = models.TextField("Vad ser ni mest fram emot?")
+    look_forward_to = models.TextField("Vad ser ni mest fram emot?", blank=True)
     music_type = models.TextField("Vilken typ av musik spelar ni helst?")
     best_memory = models.TextField("Bästa orkesterminnet?")
     rituals = models.TextField("Har ni några ritualer inför en konsert?")
     three_words = models.CharField("Beskriv orkestern med tre ord.",
                                    max_length=60)
     showpiece = models.CharField("Vilket är ert paradnummer? (alltså låten ni spelar allra bäst)",
-                                 max_length=60)
+                                 max_length=60, blank=True)
     best_with_sof = models.TextField("Vad är det bästa med SOF?")
-    why_orchestra = models.TextField("Varför borde alla studenter vara med i en orkester?")
+    why_orchestra = models.TextField("Varför borde alla studenter vara med i en orkester?", blank=True)
     craziest_thing = models.TextField("Vad är det galnaste som hänt på en spelning?")
-    determines_repertory = models.TextField("Vem bestämmer hur repertoaren ska se ut?")
+    determines_repertory = models.TextField("Vem bestämmer hur repertoaren ska se ut?", blank=True)
     what_do_you_do = models.TextField("""Om 30 minuter ska ni upp på scen,
                         hälften av manskapet ligger fortfarande och sover efter
-                        gårdagens bravader, vad gör ni?""")
+                        gårdagens bravader, vad gör ni?""", blank=True)
     uniform_description = models.TextField("Beskriv er uniform!")
-    dance = models.CharField("Förekommer spontandans på era spelningar?",
-                             choices=YESNO, max_length=5)
+    dance = models.TextField("Spontandans på era spelningar, ja eller nej?", blank=True)
     thing_to_bring = models.CharField("Om ni enbart får ta med en sak till SOF, vad skulle ni isåfall ta med?",
-                                      max_length=30)
-    mottos = models.TextField("Vad har orkestern för motton?")
+                                      max_length=30, blank=True)
+    mottos = models.TextField("Vad har orkestern för motton?", blank=True)
     orchestra_image = models.ImageField("Ladda upp en högupplöst bild på er orkester",
                                         upload_to='orchestra_images')
     logo_image = models.ImageField("Ladda upp en högupplöst bild på er logga",
@@ -124,7 +123,7 @@ class Person(models.Model):
     plays_kartege = models.CharField("Kommer du att gå/spela/dansa i kårtegen?",
                                      choices=YESNO, max_length=5)
     allergies = models.CharField("Eventuella allergier",
-                                   max_length=60, blank=True)
+                                 max_length=60, blank=True)
     needs_bed = models.CharField("Önskar sovplats",
                                  choices=YESNO, max_length=5)
     attend_sitting = models.CharField("""Önskar att få möjligheten att gå på sittningen på torsdagen
@@ -137,3 +136,5 @@ class Person(models.Model):
     medal = models.BooleanField("Medalj")
     bottle_opener = models.BooleanField("Kapsylöppnare")
     yoyo = models.BooleanField("Jojo (skidliftkortshållare som man kan ha kapsylöppnaren i)")
+
+    orchestra = models.ForeignKey(Orchestra)
