26bd9336d20dbb7211917c5b916db8e15b3f49f3
Niclas Olofsson <n@niclasolofsson.se>
2013-01-12 14:22:35 +0100

    Moved settings files and updated wsgi files.

--- anmalan/sof/conf/wsgi_cortege.py
+++ anmalan/sof/conf/wsgi_cortege.py
@@ -14,7 +14,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings-cortege")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sof.conf.settings_cortege")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
