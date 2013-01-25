816a89f0f3e8f62985765bd4f5d433214fc8a64c
Niclas Olofsson <n@niclasolofsson.se>
2013-01-25 16:47:17 +0100

    Minor 10/15 question edit

--- anmalan/sof/orkester/models.py
+++ anmalan/sof/orkester/models.py
@@ -227,8 +227,8 @@ class Member(models.Model):
 
     ticket_type = models.CharField("biljettyp", max_length=10, choices=TICKET_TYPES)
 
-    attends_10th_year = models.BooleanField("Detta blir det 10:e året i rad jag besöker SOF")
-    attends_25th_time = models.BooleanField("Detta blir mitt 25:e besök på SOF")
+    attends_10th_year = models.BooleanField("Detta blir det 10:e året i rad jag besöker SOF/STORK")
+    attends_25th_time = models.BooleanField("Detta blir mitt 25:e besök på SOF/STORK")
 
     plays_kartege = models.CharField("Kommer du att gå/spela/dansa i kårtegen?",
                                      choices=YESNO, max_length=5)
