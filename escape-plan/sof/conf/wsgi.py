13b8d8e91681e2a9edd22a390c9c8eb46b8269a4
Niclas Olofsson <n@niclasolofsson.se>
2013-02-24 03:39:11 +0100

    Updated wsgi file

--- escape-plan/sof/conf/wsgi.py
+++ escape-plan/sof/conf/wsgi.py
@@ -14,7 +14,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sof.conf.settings_cortege")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
