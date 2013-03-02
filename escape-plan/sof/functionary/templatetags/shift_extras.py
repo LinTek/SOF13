9c1706cab7cc8f8b4436fb30a3c665e3814c30f9
Niclas Olofsson <n@niclasolofsson.se>
2013-03-02 10:22:31 +0100

    Do not display shift sub type in email or public list

--- escape-plan/sof/functionary/templatetags/shift_extras.py
+++ escape-plan/sof/functionary/templatetags/shift_extras.py
@@ -4,5 +4,6 @@ register = template.Library()
 
 
 @register.inclusion_tag('functionary/partials/shift_title.html')
-def shift_title(shift, place_count=False):
-    return {'shift': shift, 'place_count': bool(place_count)}
+def shift_title(shift, place_count=False, sub_type=False):
+    return {'shift': shift, 'place_count': bool(place_count),
+            'sub_type': bool(sub_type)}
