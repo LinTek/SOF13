49e21a883e2f9392460de699a162b9e87901c981
Niclas Olofsson <n@niclasolofsson.se>
2013-03-07 03:29:35 +0100

    Hack for truncated ticket types

--- anmalan/sof/orkester/models.py
+++ anmalan/sof/orkester/models.py
@@ -35,8 +35,8 @@ ARRIVAL_DAYS = [('thursday', 'Torsdag'), ('friday', 'Fredag')]
 
 TICKET_TYPES = [('thursday',       'Torsdag - Söndag (625 kr)'),
                 ('friday',         'Fredag - Söndag (575 kr)'),
-                ('thursday-cheap', 'Torsdag - Söndag, utan mat och boende (415 kr)'),
-                ('friday-cheap',   'Fredag - Söndag, utan mat och boende (375 kr)'),
+                ('thursday-c', 'Torsdag - Söndag, utan mat och boende (415 kr)'),
+                ('friday-che',   'Fredag - Söndag, utan mat och boende (375 kr)'),
                 ('saturday',       'Lördag med mat, utan boende (250 kr)')]
 
 # Each choice must be a two-tuple. In this case we want the same value in
