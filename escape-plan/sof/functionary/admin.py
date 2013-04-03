2724c99f610682ec65a30af3507212349066ea96
Niclas Olofsson <n@niclasolofsson.se>
2013-04-04 01:11:10 +0200

    Customized admin for persons

--- escape-plan/sof/functionary/admin.py
+++ escape-plan/sof/functionary/admin.py
@@ -5,10 +5,16 @@ from .models import (ShiftType, ShiftSubType, Shift, Worker, WorkerRegistration,
                      Person, Visitor)
 
 
+class PersonAdmin(admin.ModelAdmin):
+    list_select_related = True
+    list_display = ('get_full_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+
+
 class WorkerRegistrationAdmin(admin.ModelAdmin):
     list_select_related = True
     list_display = ('worker', 'shift')
-    search_fields = ('worker__first_name', 'worker__last_name', 'worker__username')
+    search_fields = ('worker__first_name', 'worker__last_name', 'worker__email')
 
 
 class ShiftAdmin(admin.ModelAdmin):
@@ -18,9 +24,9 @@ class ShiftAdmin(admin.ModelAdmin):
     search_fields = ('shift_type__name', 'shift_sub_type__name')
 
 
-admin.site.register(Person)
-admin.site.register(Visitor)
-admin.site.register(Worker)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Visitor, PersonAdmin)
+admin.site.register(Worker, PersonAdmin)
 admin.site.register(WorkerRegistration, WorkerRegistrationAdmin)
 admin.site.register(ShiftType)
 admin.site.register(ShiftSubType)
