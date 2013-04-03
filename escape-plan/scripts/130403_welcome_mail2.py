67e8de984c3a31ebbbbb2ae3087b8f3f87af7e02
Niclas Olofsson <n@niclasolofsson.se>
2013-04-03 22:24:03 +0200

    Second welcome email

---
+++ escape-plan/scripts/130403_welcome_mail2.py
@@ -0,0 +1,8 @@
+#encoding: utf-8
+from sof.functionary.models import Worker
+
+for w in Worker.objects.filter(welcome_email_sent=False).order_by('first_name', 'last_name'):
+    w.send_welcome_email()
+    w.save()
+
+    print(unicode(w))
