9ab70ac9f9854e63debef7875e170bc0fadd140f
Niclas Olofsson <n@niclasolofsson.se>
2013-04-07 22:28:27 +0200

    Edited email invoice script

--- escape-plan/scripts/130408_send_invoice_emails.py
+++ escape-plan/scripts/130408_send_invoice_emails.py
@@ -1,4 +1,5 @@
 from sof.invoices.models import Invoice
 
-for invoice in Invoice.objects.filter(is_sent_as_email=False):
+for invoice in Invoice.objects.filter(is_sent_as_email=False, is_verified=True):
+    print unicode(invoice.person)
     invoice.send_as_email()
