91e74c1c11e80dafe9a4ada40bbe617a7eb2634f
Niclas Olofsson <n@niclasolofsson.se>
2013-04-07 11:27:18 +0200

    DKIM email backend

--- escape-plan/sof/utils/email.py
+++ escape-plan/sof/utils/email.py
@@ -1,7 +1,32 @@
-from django.conf import settings
+import dkim
 
 from django.core.mail import send_mail as _django_mail
 from django.template.loader import render_to_string
+from django.core.mail.backends.smtp import EmailBackend
+from django.conf import settings
+
+
+class DKIMBackend(EmailBackend):
+    def _send(self, email_message):
+        """
+        A helper method that does the actual sending + DKIM signing.
+        """
+        if not email_message.recipients():
+            return False
+        try:
+            message_string = email_message.message().as_string()
+            signature = dkim.sign(message_string,
+                                  settings.DKIM_SELECTOR,
+                                  settings.DKIM_DOMAIN,
+                                  settings.DKIM_PRIVATE_KEY)
+            self.connection.sendmail(email_message.from_email,
+                                     email_message.recipients(),
+                                     signature + message_string)
+        except:
+            if not self.fail_silently:
+                raise
+            return False
+        return True
 
 
 def send_mail(template_name, to, template_params):
@@ -9,4 +34,5 @@ def send_mail(template_name, to, template_params):
                                           template_params).replace('\n', ''),
                  message=render_to_string('%s.txt' % template_name,
                                           template_params),
-              from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=to)
+                 from_email=settings.DEFAULT_FROM_EMAIL,
+                 recipient_list=to)
