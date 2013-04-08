1f35bb3e50c285d434e74a0cbf69a49d2963aa56
Niclas Olofsson <n@niclasolofsson.se>
2013-04-08 16:57:37 +0200

    Trappan and pay buttons in details, and date in stats

--- escape-plan/sof/invoices/urls.py
+++ escape-plan/sof/invoices/urls.py
@@ -4,5 +4,8 @@ urlpatterns = patterns(
     'sof.invoices.views',
 
     url(r'^$', 'invoice_list', name='invoice_list'),
+
     url(r'^set-handed-out/(?P<pk>\d+)/$', 'set_handed_out', name='set_handed_out'),
+    url(r'^set-paid/(?P<pk>\d+)/$', 'set_paid', name='set_paid'),
+    url(r'^add-trappan/(?P<pk>\d+)/$', 'add_trappan', name='add_trappan'),
 )
--- escape-plan/sof/invoices/views.py
+++ escape-plan/sof/invoices/views.py
@@ -1,7 +1,14 @@
+import datetime
+
+from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 
+from sof.tickets.models import Ticket
+
 from .models import Invoice
+
+TRAPPAN_ID = 41
 
 
 @login_required
@@ -18,6 +25,30 @@ def set_handed_out(request, pk):
 
 @login_required
 @permission_required('tickets.add_invoice')
+def set_paid(request, pk):
+    invoice = get_object_or_404(Invoice, pk=pk)
+    invoice.payment_set.create(amount=invoice.amount, date=datetime.date.today())
+
+    return redirect('person_details', pk=invoice.person.pk)
+
+
+@login_required
+@transaction.commit_on_success
+def add_trappan(request, pk):
+    invoice = get_object_or_404(Invoice, pk=pk)
+
+    if not invoice.ticket_set.filter(ticket_type_id=TRAPPAN_ID).exists():
+        invoice.ticket_set.create(ticket_type_id=TRAPPAN_ID)
+        invoice.send_as_email()
+
+    return redirect('person_details', pk=invoice.person.pk)
+
+
+@login_required
+@permission_required('tickets.add_invoice')
 def invoice_list(request):
-    invoices = Invoice.objects.select_related('person', 'person__worker', 'person__visitor').prefetch_related('ticket_set', 'ticket_set__ticket_type', 'payment_set')
+    invoices = (Invoice.objects
+                .select_related('person', 'person__worker', 'person__visitor')
+                .prefetch_related('ticket_set', 'ticket_set__ticket_type', 'payment_set'))
+
     return render(request, 'invoices/invoice_list.html', {'invoices': invoices})
