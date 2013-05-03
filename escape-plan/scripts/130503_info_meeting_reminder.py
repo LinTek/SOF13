a7d6eb3d3758ac65773b20c6cc37caa00411b368
Niclas Olofsson <n@niclasolofsson.se>
2013-05-03 13:40:52 +0200

    Send info reminder to manually checked super och orchestra workers

--- escape-plan/scripts/130503_info_meeting_reminder.py
+++ escape-plan/scripts/130503_info_meeting_reminder.py
@@ -3,6 +3,6 @@ from sof.utils.email import send_mail
 
 
 for worker in Worker.objects.all():
-    if worker.workerregistration_set.count() > 0:
+    if worker.workerregistration_set.count() > 0 or worker.super_worker or worker.orchestra_worker:
         print unicode(worker)
         send_mail('functionary/mail/info_meeting_reminder', [worker.email], {})
