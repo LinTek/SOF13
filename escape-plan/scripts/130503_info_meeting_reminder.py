4412ff65c1d02696d882e96a918c976e6ba753f1
Niclas Olofsson <n@niclasolofsson.se>
2013-05-03 13:33:28 +0200

    Info meeting reminder

---
+++ escape-plan/scripts/130503_info_meeting_reminder.py
@@ -0,0 +1,8 @@
+from sof.functionary.models import Worker
+from sof.utils.email import send_mail
+
+
+for worker in Worker.objects.all():
+    if worker.workerregistration_set.count() > 0:
+        print unicode(worker)
+        send_mail('functionary/mail/info_meeting_reminder', [worker.email], {})
