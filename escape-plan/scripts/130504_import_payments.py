0bd3f5ab9722bd021719ddd2a2a6797d6b5ba0f6
Niclas Olofsson <n@niclasolofsson.se>
2013-05-04 20:31:55 +0200

    Payments import

---
+++ escape-plan/scripts/130504_import_payments.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+from decimal import Decimal
+import datetime
+
+from sof.invoices.models import Invoice, PaymentStatus
+
+
+with open('payments.csv', 'rU') as csvfile:
+    def error(emessage):
+        print emessage
+        print row
+
+    for row in csvfile:
+        message, _, amount = row.split(';')[:3]
+        message = message.strip().lower()
+
+        if not (len(message) == 14 and message.startswith('sof-')):
+            error("Unparsable payment row")
+            continue
+
+        amount = Decimal(amount)
+
+        try:
+            invoice = Invoice.objects.get(ocr=message)
+
+        except Invoice.DoesNotExist:
+            error("Could not find invoice")
+            continue
+
+        if invoice.get_payment_status() == PaymentStatus.PAID:
+            continue
+
+        invoice.payment_set.create(date=datetime.date.today(),
+                                   amount=amount)
