25e9caeddb7366a72252fc2cebed1f87a7bf5e2e
Niclas Olofsson <n@niclasolofsson.se>
2013-05-05 08:58:18 +0200

    Meep

--- escape-plan/scripts/130505_gadgets_info_email.py
+++ escape-plan/scripts/130505_gadgets_info_email.py
@@ -4,6 +4,6 @@ from sof.utils.email import send_mail
 
 
 for w in Worker.objects.filter(fetched_merchandise=False):
-    if worker.workerregistration_set.count() > 0:
+    if w.workerregistration_set.count() > 0:
         print(unicode(w))
         send_mail('functionary/mail/gadgets_info', [w.email], {})
